a6c6f63e4109dbfeef5d866700f4a7fe89de80d6
matheus13f <matheus-goes13@hotmail.com>
2021-02-28 19:44:53 -0300

    adicionado login com github, para personalizacao do uso

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,64 @@
 import { useContext, useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiEdit } from 'react-icons/fi';
 
 import { ChallengesContext } from '../context/ChallengesContext';
 import style from '../styles/components/Profile.module.css';
 
+interface IUserGithubProps {
+    name: string;
+    avatar_url: string;
+}
+
 export function Profile() {
-    const { level } = useContext(ChallengesContext);
     const [userInput, setUserInput] = useState('Unknown');
     const [hasUser, setHasUser] = useState(false);
+    const [dataGithub, setDataGithub] = useState<IUserGithubProps>();
+    const [urlPhoto, setUrlPhoto] = useState('https://github.githubassets.com/images/modules/logos_page/Octocat.png');
+
+    useEffect(() => {
+        Cookies.set('name', userInput);
+        Cookies.set('avatar', urlPhoto);
+    }, [userInput])
+
+    const { level } = useContext(ChallengesContext);
+
+    async function githubLogin() {
+        const response = await fetch(`https://api.github.com/users/${userInput}`);
+        const data = await response.json();
+        if(response.status === 200) {
+            setDataGithub(data);
+            setUrlPhoto(data.avatar_url);
+            setUserInput(data.name);
+        } else {
+            console.log('nao encontrado');
+        }
+    }
+
+    function editUserGithub() {
+        setHasUser(false);
+        setUrlPhoto('https://github.githubassets.com/images/modules/logos_page/Octocat.png');
+    }
 
     return (
         <div className={style.profileContainer} >
-            <img src="https://www.flaticon.com/svg/vstatic/svg/25/25231.svg?token=exp=1614543944~hmac=3bad9b9b50be54332e29b2c4500d3c5d" alt="Github logo"/>
+            <img src={urlPhoto} alt="Github logo"/>
             <div>
                 <strong>
                     {hasUser ? (
-                        userInput
+                        <>
+                            {userInput}
+                            <button onClick={() => editUserGithub()}> <FiEdit/></button>
+                        </>
                     ) : (
-                        <form onSubmit={() => setHasUser(true)}>
+                        <form onSubmit={() => {
+                            setHasUser(true);
+                            githubLogin();
+                        }}>
                             <input 
                                 required 
                                 type="texte" 
-                                placeholder="Digite seu nome aqui" 
+                                placeholder="Digite seu Github User ou seu nome" 
                                 onChange={(e) => setUserInput(e.target.value)} 
                             />
                             <button> <FiLogIn/></button>
