b783cd972320a2baa5ba3e43daec648dfd5d1636
matheus13f <matheus-goes13@hotmail.com>
2021-02-28 22:16:39 -0300

    time ajustado

--- src/context/CountdownContext.tsx
+++ src/context/CountdownContext.tsx
@@ -22,7 +22,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: ICountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
   
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
